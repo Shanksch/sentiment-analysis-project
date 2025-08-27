c41ac12e9d0ece9729645d54820e1052dcd16eb8
shashank chauhan <shashankchauhan8577@gmail.com>
2025-08-28 00:14:20 +0530

    ui docker file created

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:5000/predict"
+API_URL = "https://sentiment-analysis-project-iveb.onrender.com/predict"
 
 st.title("Sentiment Analysis of Text Reviews")
 
